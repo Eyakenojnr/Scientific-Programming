44708c25a3de7886a258b8dd650cf743fa83e93d
Uduak Umanah <96398168+Eyakenojnr@users.noreply.github.com>
2023-03-24 13:25:32 +0100

    commented code

--- CH1-prime_numbers/arithmetic/Exercise_1-1.cpp
+++ CH1-prime_numbers/arithmetic/Exercise_1-1.cpp
@@ -8,10 +8,15 @@ using namespace std;
 int main()
 {
     int num_1 = 0, num_2 = 0, mod = 0;
+    
+    // get in put
     cout<<"First number: ";
     cin>>num_1;
     cout<<"Second number: ";
     cin>>num_2;
-    mod = num_1 % num_2;
-    cout<<"\nThe modulus is: "<<mod<<endl;
+    
+    mod = num_1 % num_2; // modulus of both numbers
+    cout<<"\nThe modulus is: "<<mod<<endl; // print out modulus
+    
+    return 0;
 }
